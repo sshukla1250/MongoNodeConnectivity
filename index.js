52df65d03248d1087f1822b2a7dc80a3453d1f12
shukla1250 <sshukla1250@gmail.com>
2023-10-04 23:13:55 +0530

    add mongdb connectivity

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log('Hello world!');
-console.log('Hello world!');
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'system';
+
+async function getData(){
+    let result = await client.connect();
+    let db = result.db(dbName);
+    let collection = db.collection('college');
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
+getData();
